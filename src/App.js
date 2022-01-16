da81a3f47e7c075e60218b222ae7bc9b4a4914a7
Maryna-Bazylevska <83463971+Maryna-Bazylevska@users.noreply.github.com>
2022-01-16 15:40:53 +0100

    correction

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-//import React, { Component } from "react";
 import { useState } from "react";
-
 import "./App.css";
 import FeedbackOptions from "./Components/FeedbackOptions";
 import Statistics from "./Components/Statistics";
@@ -51,43 +49,3 @@ export default function App() {
     </div>
   );
 }
-
-// class oldApp extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-//   onLeaveFeedback = (key) => {
-//     this.setState((state) => ({
-//       [key]: state[key] + 1,
-//     }));
-//   };
-//   countTotalFeedback = () => {
-//     return this.state.good + this.state.neutral + this.state.bad;
-//   };
-//   countPositiveFeedbackPercentage = () => {
-//     return Math.round((this.state.good / this.countTotalFeedback()) * 100);
-//   };
-//   render() {
-//     const { good, neutral, bad } = this.state;
-//     const keyName = Object.keys(this.state);
-//     return (
-//       <div>
-//         <Section title="Please leave feedback">
-//           <FeedbackOptions
-//             options={keyName}
-//             onLeaveFeedback={this.onLeaveFeedback}
-//           />
-//           <Statistics
-//             good={good}
-//             neutral={neutral}
-//             bad={bad}
-//             total={this.countTotalFeedback()}
-//             positivePercentage={this.countPositiveFeedbackPercentage()}
-//           />
-//         </Section>
-//       </div>
-//     );
-//   }
-// }
